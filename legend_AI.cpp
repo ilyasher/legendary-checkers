0e584a734b4696afad7ae93877aea3c30f7688f1
ilyasher <ilyasherstyuk@gmail.com>
2019-12-29 16:47:37 -1000

    added a few mc functions, as well as random number generator

--- legend_AI.cpp
+++ legend_AI.cpp
@@ -7,6 +7,7 @@ LegendAI::PositionTreeNode::PositionTreeNode(const Position *pos) {
 LegendAI::PositionTreeNode::PositionTreeNode(const Position *pos, const PositionTreeNode *parent) {
     this->position = pos;
     this->parent = parent;
+    this->num_moves = pos->get_all_legal_moves().size();
 }
 
 score_t LegendAI::eval(const Position &pos) {
@@ -32,13 +33,45 @@ score_t LegendAI::eval(const Position &pos) {
     return score;
 }
 
-LegendAI::Move best_move(const Position &pos) {
+LegendAI::PositionTreeNode *LegendAI::expand_node(PositionTreeNode *node) {
+
+    // THIS IS SOMETHING THAT SHOULD BE CACHED !!!
+    std::vector<Move> all_moves = node->position->get_all_legal_moves();
+
+    // TODO: Make this random.
+    Move move = all_moves[node->children.size()];
+
+    // TODO: free these later
+    Position *new_position = new Position(node->position->play_move(move));
+    PositionTreeNode *new_node = new PositionTreeNode(new_position, node);
+
+    return new_node;
+}
+
+LegendAI::PositionTreeNode *LegendAI::select_node(PositionTreeNode *node) {
+    while (!node->is_leaf()) {
+        if (!node->is_fully_expanded()) {
+            return expand_node(node);
+        }
+        // node = best_child();
+    }
+
+}
+
+Move LegendAI::best_move(const Position &pos) {
 
     game_tree = PositionTreeNode(&pos);
 
-    // later, replace with `for 1 second`
+    // TODO: replace with `for 1 second`
     for (int i = 0; i < 100; i ++) {
-        
+        PositionTreeNode *node = select_node(&game_tree);
     }
+}
 
+/* https://stackoverflow.com/questions/7560114/random-number-c-in-some-range */
+int rand_range(int max) {
+    std::random_device rd; // obtain a random number from hardware
+    std::mt19937 eng(rd()); // seed the generator
+    std::uniform_int_distribution<> distr(0, max); // define the range
+    return distr(eng);
 }
--- legend_AI.h
+++ legend_AI.h
@@ -1,6 +1,7 @@
 #ifndef LEGEND_AI_H
 #define LEGEND_AI_H
 
+#include <random>
 #include "position.h"
 
 using score_t = int32_t;
@@ -12,14 +13,22 @@ class LegendAI {
     struct PositionTreeNode {
         const PositionTreeNode *parent;
         const Position *position;
+        int num_moves;
+
         std::vector<PositionTreeNode *> children;
 
         PositionTreeNode(const Position *pos);
         PositionTreeNode(const Position *pos, const PositionTreeNode *parent);
+
+        inline bool is_leaf() { return children.empty(); }
+        inline bool is_fully_expanded() { return children.size() == num_moves; }
     };
 
     PositionTreeNode game_tree;
 
+    PositionTreeNode *select_node(PositionTreeNode *node);
+
+    PositionTreeNode *expand_node(PositionTreeNode *node);
 
 
 public:
@@ -29,5 +38,7 @@ public:
     Move best_move(const Position &pos);
 
 };
+
+int rand_range(int max);
 
 #endif // LEGEND_AI_H
